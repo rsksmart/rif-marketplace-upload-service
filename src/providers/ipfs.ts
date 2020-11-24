80c84d2f64df276499ad785aa1ea39085f0e371e
Nazar Duchak <nazar@iovlabs.org>
2020-11-24 15:49:03 +0200

    chore: fix linter

--- src/providers/ipfs.ts
+++ src/providers/ipfs.ts
@@ -50,7 +50,7 @@ export class IpfsProvider implements Provider {
     }
 
     const ipfs = ipfsClient(options)
-// TODO remove when this API officially supported
+    // TODO remove when this API officially supported
     ipfs.dag = { stat: getDagStat(typeof options === 'string' ? options : options.url as string) }
 
     let versionObject: Version
