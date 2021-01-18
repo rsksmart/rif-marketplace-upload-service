c88d262a946d2cf7bda570058ff2e1a91eef8cc6
Alvaro Fariña <farinalvaro@gmail.com>
2021-01-18 11:14:10 -0300

    feat: adds file size limit endpoint (#99)
    * feat: adds file size limit endpoint
    
    * docs: documents file size limit endpoint
    
    Co-authored-by: Nazar Duchak <41945483+nduchak@users.noreply.github.com>

---
+++ src/upload/getSizeLimit.handler.ts
@@ -0,0 +1,14 @@
+import config from 'config'
+import { loggingFactory } from '../logger'
+
+const logger = loggingFactory('getSizeLimit.handler')
+type GetSizeLimitRouteHandler = (req: any, res: any) => Promise<void>
+
+export default function (): GetSizeLimitRouteHandler {
+  return (_: any, res: any): Promise<void> => {
+    logger.info('Retrieving file size limit')
+    return res.json({
+      fileSizeLimit: config.get<number>('fileSizeLimit')
+    })
+  }
+}
